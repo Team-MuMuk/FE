7425920e1d5ba8c13b701c6c5d33ba736e369e37
answjddn0607 <166084634+answjddn0607@users.noreply.github.com>
2025-07-14 12:38:31 +0900

    Design: 추가 info 버튼

--- app/src/main/java/com/example/mumuk/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mumuk/ui/home/HomeFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.PopupWindow
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.mumuk.R
 import com.example.mumuk.data.model.Recipe
 import com.example.mumuk.databinding.FragmentHomeBinding
+import com.google.android.material.card.MaterialCardView
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
@@ -33,10 +36,38 @@ class HomeFragment : Fragment() {
             findNavController().navigate(R.id.action_navigation_home_to_recommendFragment)
         }
 
+        binding.infoBtn.setOnClickListener {
+            showInfoPopup(it)
+        }
+
         setupRecyclerView(binding.todayRV, recipeRepository.getTodayRecipes())
         setupRecyclerView(binding.recentRV, recipeRepository.getRecentRecipes())
         setupRecyclerView(binding.healthRV, recipeRepository.getHealthRecipes())
         setupRecyclerView(binding.recommendedRV, recipeRepository.getRecommendedRecipes())
+    }
+
+    private fun showInfoPopup(anchorView: View) {
+        val inflater = LayoutInflater.from(requireContext())
+        val popupView = inflater.inflate(R.layout.popup_info, null)
+
+        val popupWindow = PopupWindow(
+            popupView,
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            false
+        )
+
+        val okButton = popupView.findViewById<MaterialCardView>(R.id.ok_card_button)
+        okButton.setOnClickListener {
+            popupWindow.dismiss()
+        }
+
+        popupView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+        val popupWidth = popupView.measuredWidth
+        val xOffset = -popupWidth + 30
+        val yOffset = -anchorView.height
+
+        popupWindow.showAsDropDown(anchorView, xOffset, yOffset)
     }
 
     private fun setupRecyclerView(recyclerView: RecyclerView, recipeList: List<Recipe>) {
