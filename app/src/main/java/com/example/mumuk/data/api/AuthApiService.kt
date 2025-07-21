85407a2d5dc71331d66e64a8f6d0f167b0c293aa
yeju321qaz <yeju321qaz@naver.com>
2025-07-21 17:26:10 +0900

    회원가입 API 연결

--- app/src/main/java/com/example/mumuk/data/api/AuthApiService.kt
+++ app/src/main/java/com/example/mumuk/data/api/AuthApiService.kt
@@ -1,7 +1,10 @@
 package com.example.mumuk.data.api
 
+
 import com.example.mumuk.data.model.auth.LoginRequest
 import com.example.mumuk.data.model.auth.LoginResponse
+import com.example.mumuk.data.model.auth.SignupRequest
+import com.example.mumuk.data.model.auth.SignupResponse
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
@@ -9,4 +12,7 @@ import retrofit2.http.POST
 interface AuthApiService {
     @POST("/api/auth/login")
     fun login(@Body request: LoginRequest): Call<LoginResponse>
+    @POST("/api/auth/sign-up")
+    fun signUp(@Body request: SignupRequest): Call<SignupResponse>
+
 }
--- app/src/main/java/com/example/mumuk/data/model/auth/SignupResponse.kt
+++ app/src/main/java/com/example/mumuk/data/model/auth/SignupResponse.kt
@@ -1,6 +1,6 @@
-package com.example.mumuk.data.model
+package com.example.mumuk.data.model.auth
 
-data class SignUpResponse(
+data class SignupResponse(
     val status: String,
     val code: String,
     val message: String,
